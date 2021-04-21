97361b96fe69925bc30047e6fc11cf6eaaaeb418
IamDreamcatcher <OlegVolkovskiy2002@yandex.ru>
2021-04-21 18:32:53 +0300

    add balance

--- 5.2 Binary tree/tree.h
+++ 5.2 Binary tree/tree.h
@@ -9,14 +9,72 @@ class tree{
             int key;
             node* left;
             node* right;
+            unsigned char height;
             node(int value) {
                 key = value;
                 left = right = nullptr;
+                height = 1;
             }
         };
         node* Root;
         size_t size;
 
+        unsigned char height(node* cur){
+            return cur ? cur -> height : 0;
+        }
+        int bfactor(node* cur){
+            return height(cur -> right) - height(cur -> left);
+        }
+        void fixheight(node* cur){
+            unsigned char hl = height(cur -> left);
+            unsigned char hr = height(cur -> right);
+            cur -> height = (hl > hr ? hl : hr) + 1;
+        }
+
+        node* rotateright(node* cur){
+            node* child = cur -> left;
+            cur -> left = child -> right;
+            child -> right = cur;
+            fixheight(cur);
+            fixheight(child);
+            return child;
+        }
+        node* rotateleft(node* cur){
+            node* child = cur -> right;
+            cur -> right = child -> left;
+            child -> left = cur;
+            fixheight(cur);
+            fixheight(child);
+            return child;
+        }
+
+        node* balance(node* cur){
+            fixheight(cur);
+            if(bfactor(cur) == 2){
+                if(bfactor(cur -> right) < 0){
+                    cur -> right = rotateright(cur -> right);
+                }
+                return rotateleft(cur);
+            }
+            if(bfactor(cur) == -2){
+                if(bfactor(cur -> left) > 0){
+                    cur -> left = rotateleft(cur -> left);
+                }
+                return rotateright(cur);
+            }
+            return cur;
+        }
+
+        node* findmin(node* cur) {
+            return cur -> left ? findmin(cur -> left) : cur;
+        }
+        node* removemin(node* cur) {
+            if(cur -> left == 0) {
+                return cur -> right;
+            }
+            cur -> left = removemin(cur -> left);
+            return balance(cur);
+        }
         node* insert(node* cur, int value) {
             if (!cur) {
                 size++;
@@ -28,7 +86,7 @@ class tree{
             else {
                 cur -> right = insert(cur -> right, value);
             }
-            return cur;
+            return balance(cur);
         }
         node* erase(node* cur, int value) {
             if(!cur) {
@@ -51,28 +109,28 @@ class tree{
                 node* min = findmin(r);
                 min -> right = removemin(r);
                 min -> left = q;
-                return min;
+                return balance(min);
+            }
+            return balance(cur);
+        }
+        node* remove_negative_numbers(node* cur) {
+            if (cur -> left != nullptr) {
+                cur -> left = remove_negative_numbers(cur -> left);
+            }
+            if (cur -> right != nullptr) {
+                cur -> right = remove_negative_numbers(cur -> right);
+            }
+            if (cur -> key < 0) {
+                cur = erase(cur, cur -> key);
             }
             return cur;
         }
-        node* remove_negative_numbers(node* root) {
-            if (root -> left != nullptr) {
-                root -> left = remove_negative_numbers(root -> left);
-            }
-            if (root -> right != nullptr) {
-                root -> right = remove_negative_numbers(root -> right);
-            }
-            if (root -> key < 0) {
-                root = erase(root, root -> key);
-            }
-            return root;
-        }
         void to_vector(node* cur, std::vector <int>& vec){
-            if (cur -> left != nullptr){
+            if (cur -> left != nullptr) {
                 to_vector(cur -> left, vec);
             }
             vec.push_back(cur -> key);
-            if (cur -> right != nullptr){
+            if (cur -> right != nullptr) {
                 to_vector(cur -> right, vec);
             }
         }
@@ -82,16 +140,6 @@ class tree{
         }
         void insert(int value) {
             Root = insert(Root, value);
-        }
-        node* findmin(node* cur) {
-            return cur -> left ? findmin(cur -> left) : cur;
-        }
-        node* removemin(node* cur) {
-            if(cur -> left == 0) {
-                return cur -> right;
-            }
-            cur -> left = removemin(cur -> left);
-            return cur;
         }
         void erase(int value) {
             Root = erase(Root, value);
